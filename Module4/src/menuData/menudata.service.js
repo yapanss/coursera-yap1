8c537820ba0ee1a4ef92049b445bdb14545bfd88
Assi <yapanss@yahoo.fr>
2017-03-12 00:32:30 +0000

    modify url david-chu restaurant

--- Module4/src/menuData/menudata.service.js
+++ Module4/src/menuData/menudata.service.js
@@ -7,7 +7,7 @@ MenuDataService.$inject=["$http"];
 function MenuDataService($http){
 	
 	var service=this;
-	service.getAllCategories = function(categoryShortName){
+	service.getAllCategories = function(){
 		return $http({url:"https://davids-restaurant.herokuapp.com/categories.json"})
 		.then(function(response){
 			var categories = response.data;
@@ -16,7 +16,7 @@ function MenuDataService($http){
 		});
 	};
 	service.getItemsForCategory = function(categoryShortName){
-		return $http({url:"https:davids-restaurant.herokuapp.com/menu_items.json?category="+categoryShortName})
+		return $http({url:"https://davids-restaurant.herokuapp.com/menu_items.json?category="+categoryShortName})
 		.then(function(response){
 			var items = response.data.menu_items;
 			console.log("The items are :", items);
